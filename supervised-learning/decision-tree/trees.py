571457826921abb312737dbdd1cc8916fed4952d
Le Sang <sang.le@mservice.com.vn>
2022-05-06 17:27:13 +0700

    finish decision tree

--- supervised-learning/decision-tree/trees.py
+++ supervised-learning/decision-tree/trees.py
@@ -1,4 +1,5 @@
 from math import log
+import operator
 
 def createDataSet():
 	dataSet = [[1, 1, 'yes'],
@@ -49,3 +50,28 @@ def chooseBestFeatureToSplit(dataSet):
 			bestInfoGain = infoGain
 			bestFeature = i
 	return bestFeature
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys(): classCount[vote] = 0.0
+        classCount += 1.0
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeat])
+    bestFeatValues = [example[bestFeat] for example in dataSet]
+    uniqueBestFeatValues = set(bestFeatValues)
+    for value in uniqueBestFeatValues:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+    return myTree
