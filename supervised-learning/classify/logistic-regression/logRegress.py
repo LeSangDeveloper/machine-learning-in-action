b0573346bb139159d186e92b856320492336c7e7
Le Sang <sang.le@mservice.com.vn>
2022-05-17 14:42:41 +0700

    logistic regression: plot best fit

--- supervised-learning/classify/logistic-regression/logRegress.py
+++ supervised-learning/classify/logistic-regression/logRegress.py
@@ -22,5 +22,27 @@ def gradAscent(dataMatIn, classLabels):
 	for i in range(maxCycles):
 		errors = (matLabels - sigmoid(dataMat * weights))
 		weights = weights + alpha * dataMat.transpose() * errors
-	return weight
+	return weights
 
+def plotBestFit(wei):
+    import matplotlib.pyplot as plt
+    weights = wei.getA()
+    dataMat, labelMat = loadDataSet()
+    dataArr = array(dataMat)
+    n = shape(dataArr)[0]
+    x1Cord = []; y1Cord = []
+    x2Cord = []; y2Cord = []
+    for i in range(n):
+        if (labelMat[i] == 1):
+            x1Cord.append(dataArr[i, 1]); y1Cord.append(dataArr[i, 2])
+        else:
+            x2Cord.append(dataArr[i, 1]); y2Cord.append(dataArr[i, 2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(x1Cord, y1Cord, s=30, color='red', marker='s')
+    ax.scatter(x2Cord, y2Cord, s=30, color='green')
+    x = arange(-3.0, 3.0, 0.1)
+    y = (-weights[0] - x * weights[1]) / weights[2]
+    ax.plot(x, y)
+    plt.xlabel('X1'); plt.ylabel('X2')
+    plt.show()
