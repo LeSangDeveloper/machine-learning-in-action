8604028c85764de7c7f069432b0cc5cd015d5f6a
Le Sang <sang.le@mservice.com.vn>
2022-05-27 19:38:48 +0700

    init simplified  SMO

--- supervised-learning/classify/svm/svmMLiA.py
+++ supervised-learning/classify/svm/svmMLiA.py
@@ -21,3 +21,21 @@ def clipAlpha(aj, H, L):
     if aj < L:
         aj = L
     return aj
+
+def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
+    dataMatrix = mat(dataMatIn); labelMat = mat(classLabels).tranpose()
+    m, n = shape(dataMatrix)
+    alphas = zeros(m, 1); iterTemp = 0; b = 0
+    while (iterTemp < maxIter):
+        alphaPairsChanged = 0
+        for i in range(m):
+            # f(xi) = w.xi + b
+            fXi = (multiply(alphas, labelMat).T * (dataMatrix * dataMatrix[i:].T)) + b
+            # E(i) = f(xi) - y(i)
+            Ei = fXi - float(labelMat[i])
+            if ((Ei * labelMat[i] < -toler) and (alphas[i] < C) or (Ei * labelMat[i] > toler and alphas[i] > 0)):
+                j = selectJrand(i, m)
+
+        if (alphasPairsChanged == 0): iterTemp += 1
+        else: iterTemp = 0
+    return b, alphas
